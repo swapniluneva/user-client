c7b78419dafddbb8fb7595a177e22f87b844e76a
swapnil srivastava <swapnil_srivastava@unifyed.com>
2020-10-30 22:41:11 +0530

    new pages for print and scan added

--- app/js/app.js
+++ app/js/app.js
@@ -7,6 +7,7 @@ config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     when("/", { templateUrl: "views/users.html", controller: "userController" }).
     when("/Registration/form", { templateUrl: "views/form.html", controller: "userController" }).
-    when("/Registration/PrintRegistrationFormVehicleCheckValidOrNot.aspx", { templateUrl: "views/print.html", controller: "userController" }).
+    when("/Registration/print", { templateUrl: "views/print_final.html", controller: "userController" }).
+    when("/Registration/PrintRegistrationFormVehicleCheckValidOrNot.aspx", { templateUrl: "views/scan.html", controller: "userController" }).
     otherwise({ redirectTo: '/' });
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,86 +14,86 @@ controller('userController', function($scope, userAPIService, $location) {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "1.eMM11:" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'emm11' },
                 { class: 'heading column', value: "2. Name Of Lessee /Permit Holder:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'name' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "3. Mobile Number Of Lessee:" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'mobile' },
                 { class: 'heading column', value: "4. Tin Number:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'tin' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "5. Lessee Id:" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'lessee' },
                 { class: 'heading column', value: " 6. Lease Details [Address,Village,(Gata/Khand),Area]" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'address' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "7. Tehsil Of Lease:" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'tehsil' },
                 { class: 'heading column', value: "8. District Of Lease:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'distt' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "9. QTY Transported in Cubic Meter:" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'qty' },
                 { class: 'heading column', value: "10. Name Of Mineral:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'mineral' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "11. Loading From:" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'loading' },
                 { class: 'heading column', value: "12. Destination (Delivery Address):" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'dest' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "13. Distance(Approx in K.M.):" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'dist' },
                 { class: 'heading column', value: "14. eTP Generated On:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'etp' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "15. eMM11 Valid Upto:" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'valid' },
                 { class: 'heading column', value: "16. Traveling Duration:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'duration' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "17. Destination District:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'destination' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "18.Pits Mouth Value(Rs/m3 &Rs/Ton for Silica sand):" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'pmv' }
             ]
         },
         {
@@ -106,50 +106,50 @@ controller('userController', function($scope, userAPIService, $location) {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "1. Registration Number :" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'reg' },
                 { class: 'heading column', value: "2. Type Of Vehicle:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'type' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "3. Name Of Driver :" },
-                { class: 'column', type: 'input', value: '' },
+                { class: 'column', type: 'input', value: '', name: 'driver' },
                 { class: 'heading column', value: "4. Mobile Number Of Driver:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'driverMobile' }
             ]
         },
         {
             colspan: 1,
             cols: [
                 { class: 'heading column', value: "5. DL Number Of Driver:" },
-                { class: 'column', type: 'input', value: '' }
+                { class: 'column', type: 'input', value: '', name: 'dl' }
             ]
         },
         {
             colspan: 2,
             cols: [
                 { class: 'heading1 column', value: "This eMM11 is valid up to:-" },
-                { class: 'heading2 column', class: 'column', type: 'input', value: "" }
+                { class: 'heading2 column', class: 'column', type: 'input', value: "", name: 'emm11Upto' }
             ]
         }
     ];
 
+    $scope.printData = {};
     ((eId) => {
         if (eId) {
             userAPIService.getData(eId).success(function(response) {
-                console.log('response', response);
-                $scope.formData = response.data.clientData;
+                $scope.printData = response.data.clientData;
             });
-            $scope.codeUrl = userAPIService.getQRUrl(window.location.href);
+            $scope.codeUrl = userAPIService.getQRUrl(window.location.origin + '/app/#/Registration/PrintRegistrationFormVehicleCheckValidOrNot.aspx?eId=' + eId);
         }
     })(eId)
 
     $scope.addData = () => {
-        console.log('$scope.formData', $scope.formData);
-        userAPIService.addData($scope.formData).success(function(response) {
-            $location.path('/Registration/PrintRegistrationFormVehicleCheckValidOrNot.aspx').search("eId", response.data);
+        console.log('$scope.formData', $scope.formData, $scope.printData);
+        userAPIService.addData($scope.printData).success(function(response) {
+            $location.path('/Registration/print').search("eId", response.data);
 
         });
     }
